4e79a24eb1d9b98eff5860ad049f0d0da6b8dc83
NadezdhaDekhand <nadeshka7979@gmail.com>
2021-12-16 23:14:07 +0700

    ДЗ №5

--- src/main/java/org/example/AddToCart.java
+++ src/main/java/org/example/AddToCart.java
@@ -1,12 +1,11 @@
 package org.example;
 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 
 public class AddToCart
 {
